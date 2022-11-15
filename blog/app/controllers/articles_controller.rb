e71a4d6098df068904ffb8e6d58ea7f00e879031
EC2 Default User <ec2-user@ip-10-16-0-211.ec2.internal>
2022-11-15 23:21:08 +0000

    up to step 5.11 completed

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,17 +1,38 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
  
   def new
+  @article = Article.new
+  end
+  
+  def edit
+  @article = Article.find(params[:id])
   end
  
 def create
-  @article = Article.new(params.require(:article).permit(:title, :text))
   @article = Article.new(article_params)
  
-  @article.save
-  redirect_to @article
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+end
+
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
 end
  
 private
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,2 +1,6 @@
+
 class Article < ApplicationRecord
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
+
