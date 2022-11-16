db8b177dd1901ca9743a3083c1b3140076941931
EC2 Default User <ec2-user@ip-10-16-0-211.ec2.internal>
2022-11-16 04:16:11 +0000

    up to step 6

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -34,6 +34,13 @@ def update
     render 'edit'
   end
 end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
  
 private
   def article_params
