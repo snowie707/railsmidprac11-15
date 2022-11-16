83aed1a9b3a33267c4f6c794b2e2865883c1c330
EC2 Default User <ec2-user@ip-10-16-0-211.ec2.internal>
2022-11-16 04:57:31 +0000

    up to step 7

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
  
   
-  resources :articles
+resources :articles do
+  resources :comments
+ end
   
    root 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+    end 
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmend
