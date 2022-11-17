b5555b7afc85287e9530d0af60f70c1bf539741f
EC2 Default User <ec2-user@ip-10-16-0-13.ec2.internal>
2022-11-17 21:17:20 +0000

    duplication fixed

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -6,24 +6,20 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   
-  def index
-    @articles = Article.all
-  end
-  
   def show
     @article = Article.find(params[:id])
   end
  
   def new
-  @article = Article.new
+    @article = Article.new
   end
   
   def edit
-  @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
  
 def create
-  @article = Article.new(article_params)
+    @article = Article.new(article_params)
  
   if @article.save
     redirect_to @article
